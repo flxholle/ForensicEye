7186f32c2e8facd1d2fe897e1702fb56721333fe
Felix Hollederer <flxholle@posteo.com>
2025-09-14 22:17:34 +0200

    enable bluetooth

--- auto_run.sh
+++ auto_run.sh
@@ -17,13 +17,15 @@ echo "Starting Forensic Eye Auto Run..."
 echo "Make sure that USB Debugging is enabled on your Android device."
 
 
-if adb shell pm list packages | grep -q "com.flxholle.forensiceye"; then
-    echo "Uninstalling existing Forensic Eye app..."
-    adb uninstall com.flxholle.forensiceye
+if ! adb shell pm list packages | grep -q "com.flxholle.forensiceye"; then
+    echo "Installing Forensic Eye APK..."
+    adb install -g forensiceye.apk
+else
+    echo "Forensic Eye app is already installed. Skipping installation."
 fi
 
-echo "Installing Forensic Eye APK..."
-adb install -g forensiceye.apk
+echo "Enabling Bluetooth..."
+adb shell cmd bluetooth_manager enable
 
 echo "Launching Forensic Eye Auto Run..."
 adb shell am start -n com.flxholle.forensiceye/.AutoRunActivity
